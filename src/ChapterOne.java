f53ee003de436b72b2d7394f6d6b52d7d311e197
David <davsucks@umich.edu>
2015-03-30 17:51:27 -0400

    Complete chapter one

---
+++ src/ChapterOne.java
@@ -0,0 +1,13 @@
+/**
+ * Print the sum of all odd numbers from 1 to 100
+ *
+ * Created by David on 3/30/2015.
+ */
+
+public class ChapterOne {
+    public static void main(String[] args) {
+        for (int i = 1; i < 101; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
